37f7aeaeba3378a090a5a1d2c5de22c41a9231de
m9139 <m.sli2012@ya.ru>
2024-03-16 20:27:27 +0700

    01.01

--- decoration.py
+++ decoration.py
@@ -1,20 +1,14 @@
-def is_prime(func):
-    def wrapper(*args, **kwargs):
-        res = func(*args, **kwargs)
-        if res % 2 == 0:
-            ent = 'Простое'
-        else:
-            ent = 'Составное'
-
-            print(ent)
-            return res
-    return wrapper
+def is_prime(x):
+    def wrapper(number):
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                print('простое')
+            else:
+                print('составное')
+        return wrapper
 
 
 @is_prime
 def sum_of_three(a, b, c):
-    return a + b + c
-
-
-result = sum_of_three(54, 23, 12)
-print(result)
+    res = 4 + 6 + 8
+    print(res)
